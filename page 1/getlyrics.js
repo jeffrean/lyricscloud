213d7dfca0f04c73c105ed3c090717d470ec6f70
bryce <bryce@bryces-MacBook-Pro-2.local>
2017-03-01 17:36:26 -0800

    lyrics works

--- page 1/getlyrics.js
+++ page 1/getlyrics.js
@@ -1,18 +1,30 @@
 var lyrics = [];
 
-function getLyricsUrl(artist, song) {
-    return "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=" + song + "&q_artist=" + artist + "&apikey=902908a8c199f254a1b29d864f9398a4";
-}
+    function requestLyrics(artist, currentSong) {
+    $.ajax({
+      url: 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?',
+      data: {
+        q_track: currentSong,
+        q_artist: artist,
+        format: 'jsonp',
+        apikey: '902908a8c199f254a1b29d864f9398a4',
+      },
+      dataType: 'jsonp' ,
+      jsonp: 'callback',
+      success: function (json){
+        console.log('callback function');
+        console.log(3);
+        console.log(json);
+        console.log(4);
+        parseLyrics(json,currentSong);
+        console.log(33);
+        console.log(lyrics);
+        console.log(lyrics.length);
+        console.log(44);
+      },
+    });
 
-function requestLyrics(url, currentSong) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            parseLyrics(JSON.parse(xhr.responseText.substring(9, xhr.responseText.length - 2)), currentSong);
-        }
-    }
-    xhr.open('GET', url, true);
-    xhr.send(null);
+
 }
 
 function parseLyrics(body, currentSong) {
@@ -21,16 +33,12 @@ function parseLyrics(body, currentSong) {
             "title": currentSong,
             "lyrics": lyric.split("*")[0]
         });
-        console.log(lyrics);
 }
 
 function getLyricsList(artist, songs) {
     var currentSong;
     for (var i = 0; i < songs.length; i++) {
         currentSong = songs[i];
-        requestLyrics(getLyricsUrl(artist, currentSong), currentSong);
+        requestLyrics(artist, currentSong);
     }
-
-    setTimeout(function(){}, 1000);
-    return lyrics;
 }
