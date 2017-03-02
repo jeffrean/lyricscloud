a8f1b5ec22c127cbc244fadefe71dfa419c87549
mihyunan <mihyunan@usc.edu>
2017-03-01 20:49:29 -0800

    cleaned up getArtists.php

--- wordcloud/getArtists.php
+++ wordcloud/getArtists.php
@@ -1,7 +1,14 @@
-<?php 
+<?php
+	
+	//Obtains artists and artist images from spotify.com
+	//The obtained data is put into an associative array structure and 
+	//encoded into a JSON format
+
 	$artist = $_GET["artist"];
 	$url = "https://api.spotify.com/v1/search?q=".$artist."&type=artist";
 	
+	
+	//setting options for cURL transfer
 	$ch = curl_init();
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -10,36 +17,26 @@
     $result = curl_exec($ch);
     curl_close($ch);
 	
-	
-	//$response = file_get_contents($ch);
 	$json_response = json_decode($result, true);
-	//var_dump($json_response['artists']['items'][0]['images'][0]['url']);
 	
 	$artists_json = $json_response['artists']['items'];
-	
 	$artists_data = array();
 	
-	
-	for ($i =0; $i < count($artists_json); $i++) {
+	//iterating through artists and putting name and images to the array
+	for ($i = 0; $i < count($artists_json); $i++) {
 		$artists_images = array();
 		$artists_images["name"] = $artists_json[$i]['name'];
-		$artists_images["url"] = $artists_json[$i]['images'][0]['url'];
+		$artists_images["image_url"] = $artists_json[$i]['images'][0]['url'];
 		$artists_images["popularity"] = $artists_json[$i]['popularity'];
 		
 		array_push($artists_data, $artists_images);
 	}
 	
+	//sorting the array by popularity in descending order
 	usort($artists_data, function($a, $b) {
 		return $b['popularity'] - $a['popularity'];
 	});
 	
-	
 	echo json_encode($artists_data, JSON_UNESCAPED_SLASHES);
-	//console.log($json_response);
-	
-	/*foreach ($json_response['artists']['items']['images'] as $images) {
-		$image = $images['url'];
-		var_dump($image);
-	}*/
 	
 ?>
